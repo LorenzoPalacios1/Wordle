8b7a0bc1f2990a6d6341b30a2f2d4ebddb614161
Lorenzo Palacios <lp434717@gmail.com>
2023-05-15 09:19:03 -0500

    sync

--- RandomWord.java
+++ RandomWord.java
@@ -7,21 +7,20 @@ public class RandomWord {
     public static String generateWord() throws IOException {
         final URLConnection wordSource = new URL("https://random-word-api.herokuapp.com/word?length=5")
                 .openConnection();
-        wordSource.setReadTimeout(3000);
+        wordSource.setReadTimeout(5000);
 
         final InputStream word = wordSource.getInputStream();
 
-        // Retrieving raw word
-        byte data[] = word.readAllBytes();
-
         /*
          * The raw returned word contains two prefixed and suffixed brackets and double
-         * quotes, so in the below for-loop we start at an index of 2 and end 2 early
+         * quotes, so we discard the first two bytes.
          */
-        String formattedWord = "";
+        word.read();
+        word.read();
 
-        for (int i = 2; i < data.length - 2; i++) {
-            formattedWord += (char) data[i];
+        String formattedWord = "";
+        for (int i = 0; i < 5; i++) {
+            formattedWord += (char) word.read();
         }
 
         // Returning word
@@ -35,23 +34,30 @@ public class RandomWord {
     public static void main(String[] args) throws IOException {
         String words[] = new String[5];
 
+        boolean failed;
         for (int i = 0; i < words.length; i++) {
+            failed = false;
             words[i] = generateWord();
 
-            if (words[i].isEmpty()) {
-                System.err.printf("Test %d string is blank\n", i);
+            if (words[i] == null) {
+                System.err.printf("Test %d returned null\n", i);
                 continue;
             }
 
-            if (words[i].length() != 5)
+            if (words[i].length() != 5) {
                 System.err.printf("Test %d returned string length not equal to 5\nReturned data: %s\n", i, words[i]);
+                failed = true;
+            }
 
-            if (!words[i].matches("[^a-zA-Z]"))
+            if (!words[i].matches("[a-zA-Z]+")) {
                 System.err.printf("Test %d returned non-alphabetical characters\nReturned data: %s\n", i, words[i]);
+                failed = true;
+            }
+
+            if (!failed)
+                System.out.printf("Test %d success: %s\n", i, words[i]);
         }
+
+        System.out.println("Tests complete");
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 3f6813a6ebf87d353719c1943bde9de36df466c4
--- test.java
+++ test.java
@@ -2,43 +2,52 @@ import javax.swing.*;
 import javax.swing.text.MaskFormatter;
 import java.awt.*;
 import java.awt.event.*;
-import java.io.IOException;
 import java.text.ParseException;
 
 public class test extends JFrame implements ActionListener {
-    private JPanel panel;
+    private JPanel panelMain;
 
-    private JFormattedTextField text;
-    private MaskFormatter charOnly;
-
-    public static void main(String[] args) throws ParseException, IOException {
+    public static void main(String[] args) throws ParseException {
         JFrame frame = new test();
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        System.out.println(RandomWord.generateWord());
+    }
+
+    private void createCharBoxRow(int numBoxes) throws ParseException {
+        final MaskFormatter charOnlyFormatter = new MaskFormatter("?");
+        final GridLayout layout = new GridLayout(1, 5);
+        final Dimension panelSize = new Dimension(75, 30);
+
+        JPanel charBoxPanel = new JPanel(layout);
+        charBoxPanel.setPreferredSize(panelSize);
+
+        JFormattedTextField newCharBox;
+        for (int i = 0; i < numBoxes; i++) {
+            newCharBox = new JFormattedTextField(charOnlyFormatter);
+            newCharBox.setPreferredSize(new Dimension(25, 25));
+
+            charBoxPanel.add(newCharBox);
+            newCharBox.addActionListener(this);
+        }
+        panelMain.add(charBoxPanel);
     }
 
     test() throws ParseException {
         super("char box test");
 
-        panel = new JPanel();
-        panel.setPreferredSize(new Dimension(600, 400));
+        panelMain = new JPanel();
+        panelMain.setPreferredSize(new Dimension(600, 400));
 
         // Creating char-only input boxes
-        charOnly = new MaskFormatter("?");
         for (int i = 0; i < 5; i++) {
-            text = new JFormattedTextField(charOnly);
-            text.setPreferredSize(new Dimension(25, 25));
-
-            panel.add(text);
-            text.addActionListener(this);
+            createCharBoxRow(5);
         }
 
-        setContentPane(panel);
+        setContentPane(panelMain);
     }
 
     public void actionPerformed(ActionEvent event) {
-        System.out.println(event);
+        System.out.println(event.getActionCommand());
     }
 }
